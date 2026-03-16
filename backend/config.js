9bc85888125bdada89c696129a082fa3d2ab9b1a
Diwakar Singh <diwakarsi9090@gmail.com>
2026-03-17 01:52:44 +0530

    Some Route Adjustments

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+const JWT_SECRET = "paytm-clone-secret-key";
+
+module.exports = JWT_SECRET
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const cors = require("cors");
+
+app.use(cors());
+app,use(express.json());
 const mainRouter = require("./routes/index");
+const { use } = require("react");
 
 const app = express();
 app.use("/api/v1", mainRouter);
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
  // /api/v1/user/signup 
  // /api/v1/user/signin 
  // /api/v1/user/changePassword 
