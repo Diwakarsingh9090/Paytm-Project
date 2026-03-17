23f9ae6853fa714fb9d0ba2a6802f907ab9cfb54
Diwakar Singh <diwakarsi9090@gmail.com>
2026-03-18 02:21:19 +0530

    Removed config.js from tracking

--- backend/config.js
+++
@@ -1,7 +0,0 @@
-const JWT_SECRET = "paytm-clone-secret-key";
-const MONGO_URL = 'mongodb+srv://diwakarsingh1805:Diwakar1234@diwakar.4bzkxmy.mongodb.net/paytmData';
-
-module.exports = {
-    JWT_SECRET,
-    MONGO_URL
-};
