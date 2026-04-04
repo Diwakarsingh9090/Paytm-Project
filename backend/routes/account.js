6febfd0b33e6e5704130ced4c07531b72eec0a7a
Diwakar Singh <diwakarsi9090@gmail.com>
2026-04-04 14:26:23 +0530

    Added some notes in account.js and finishing backend

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -53,3 +53,8 @@ router.post("/transfer", authMiddleware, async (req, res) => {
 });
 
 module.exports = router;
+
+//NOTES
+// 1. We use Mongoose sessions to ensure that the transfer operation is atomic. If any part of the transaction fails, we can roll back to maintain data integrity.
+// 2. The /balance endpoint allows users to check their current balance, while the /transfer endpoint enables them to transfer funds to another user.
+// 3. We perform necessary checks to ensure that the user has sufficient balance and that the recipient account exists before proceeding with the transfer, and also ensuring that no tow or more transactions can interfere with each other, which is crucial for financial operations. With allowing single threaded operations, we can ensure that the balance is always accurate and consistent, even under high load.
